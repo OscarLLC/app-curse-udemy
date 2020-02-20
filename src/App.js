15e408a3e621ff129e4e02dbda8f36de1b4b6dab
OscarLLC <oscarlopez88991@gmail.com>
2020-02-20 18:13:51 -0500

    load total

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <div>
+          <h2>Este es el nuevo reto aprender react</h2>
+          <p>Lo aprenderemos con la ayuada del profesor he indagando 
+            en internet.
+          </p>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
