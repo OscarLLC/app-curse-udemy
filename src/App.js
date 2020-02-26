b4d6224d28b2414678be0675b4212ccb8bdccf01
OscarLLC <oscarlopez88991@gmail.com>
2020-02-26 15:09:44 -0500

    props default

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-
-// function Hello(props){
-//   return <h2>{props.title}</h2>
-// }
-
-const Hello = (props) => <h2>{props.title}</h2>
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <div>
-         <Hello title ="este es el contenido del from por props" />
-          <p>Lo aprenderemos con la ayuada del profesor he indagando 
-            en internet.
-          </p>
-       </div>
-      </header>
-    </div>
-  );
+class TextUno extends Component{
+  render(){
+    const {enenciadoUno,enenciadoDos,numero,boolean,arrayNumber } = this.props
+    return(
+      <div>
+        <p>{enenciadoUno}</p>
+        <p>{enenciadoDos}</p>
+        <p>{numero}</p>
+         <p>{boolean} </p>
+         <p>{arrayNumber.join(',')}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+class TituloDefault extends Component{
+  render(){
+    return(
+      <div>
+        <h3>{this.props.titulodefecto}</h3>
+      </div>
+    )
+    
+  }
+}
+
+TituloDefault.defaultProps ={
+  titulodefecto: "este es un texto por defecto"
+}
+
+class Text extends Component{
+  render(){
+    return(
+      <div>
+        <h1>texto principal</h1>
+        <TextUno 
+          enenciadoUno="te amo amor nunca lo olvides te amo mucho"
+          enenciadoDos="eres y seras mi unico amor te amo"
+          numero={2}
+          boolean
+          arrayNumber={[1,2,2,7]}
+        />
+         <TituloDefault />
+      </div>
+    )
+  }
+}
+
+export default Text;
